8dd48e1f33f685145a385d98ca159e154ec55d7d
karim dinani <karim.dinani@haberwater.com>
2020-08-07 21:05:16 +0530

    commit

--- quickfind.py
+++ quickfind.py
@@ -12,15 +12,19 @@ class QuickFind:
     def union(self, p, q):
         pid = self.ids[p]
         qid = self.ids[q]
-        for i in range(0, len(id)):
+        for i in range(0, len(self.ids)):
             if self.ids[i] == pid:
                 self.ids[i] = qid
 
 
 def main():
     qf = QuickFind(10)
+    print(qf.connected(1,2))
+    qf.union(1,2)
+    print(qf.connected(1,2))
 
 
 if __name__ == '__main__':
     print("this is a demo for quickfind algorithm")
     main()
+
